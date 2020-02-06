560f1025d814c624f94d94549ff13308c1aefe83
Max Krichenbauer <max@marui-plugin.com>
2020-02-06 13:36:50 +0900

    Added android aar and demo apps.

---
+++ android/MiVRyDemoContinuous/app/src/main/java/com/maruiplugin/mivrydemocontinuous/MainActivity.java
@@ -0,0 +1,317 @@
+package com.maruiplugin.mivrydemocontinuous;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.res.Resources;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.maruiplugin.mivry.MiVRy;
+import com.maruiplugin.mivry.MiVRyContinuousIdentificationListener;
+import com.maruiplugin.mivry.MiVRyTrainingListener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.lang.Runnable;
+
+public class MainActivity extends AppCompatActivity implements MiVRyContinuousIdentificationListener {
+
+    private ButtonListenerRecord button_listener_record;
+    private ButtonListenerCreate button_listener_create;
+    private ButtonListenerFinish button_listener_finish;
+    private ButtonListenerReset  button_listener_reset;
+    private TrainingListener     training_listener;
+
+    private static ConstraintLayout layout_main;
+    private static Button button_record;
+    private static Button button_create;
+    private static Button button_finish;
+    private static Button button_reset;
+    private static TextView textview_message;
+    private static TextView textview_keyword;
+    private static TextView textview_gesturelist;
+    private static TextView textview_buildstamp;
+
+    private static MiVRy mivry;
+    private static int recording_gesture_id;
+    private static double recognition_preformance;
+    private static String save_gesture_database_path;
+    private static final String[] codewords_sourcelist = {
+        "red", "blue", "green", "yellow", "orange", "pink", "teal", "brown", "grey"
+    };
+
+    private List<String> codewords = null;
+
+    static private Random prng = null;
+
+    private String getRandomCodeword() {
+        int i = prng.nextInt(codewords.size());
+        String c = codewords.get(i);
+        codewords.remove(i);
+        return c;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //save_gesture_database_path = getFilesDir().getAbsolutePath();
+        save_gesture_database_path = getExternalFilesDir(null).getAbsolutePath();
+        prng = new Random();
+        mivry = new MiVRy(this);
+        mivry.SetContinuousIdentificationPeriod(1000);
+        mivry.SetContinuousIdentificationSmoothing(3);
+
+        this.init();
+    }
+
+    private void init() {
+        this.codewords = new ArrayList<String>();
+        for (int i=0; i<codewords_sourcelist.length; i++) {
+            codewords.add(codewords_sourcelist[i]);
+        }
+
+        String c = this.getRandomCodeword();
+        this.recording_gesture_id = mivry.CreateGesture(c);
+
+        this.textview_message = findViewById(R.id.text_message);
+        textview_message.setText("Use the button below\nto record gestures.");
+        this.textview_keyword = findViewById(R.id.text_keyword);
+        textview_keyword.setText("Your keyword is: "+c);
+        this.textview_gesturelist = findViewById(R.id.text_gesturelist);
+        textview_gesturelist.setVisibility(View.INVISIBLE);
+        this.textview_buildstamp = findViewById(R.id.text_buildstamp);
+        textview_buildstamp.setText("Version: "+BuildConfig.BUILD_STAMP);
+
+        layout_main = findViewById(R.id.layout_main);
+        Resources resources = getResources();
+        layout_main.setBackgroundColor(resources.getColor(resources.getIdentifier(c, "color", getPackageName())));
+
+        button_record = findViewById(R.id.button_record);
+        this.button_listener_record = new ButtonListenerRecord();
+        button_record.setOnTouchListener(this.button_listener_record);
+        button_record.setText("Touch and hold\nand move phone\nto record gesture sample");
+
+        button_create = findViewById(R.id.button_create);
+        this.button_listener_create = new ButtonListenerCreate();
+        button_create.setOnTouchListener(this.button_listener_create);
+        button_create.setVisibility(View.INVISIBLE);
+        button_create.setEnabled(false);
+
+        button_finish = findViewById(R.id.button_finish);
+        this.button_listener_finish = new ButtonListenerFinish();
+        button_finish.setOnTouchListener(this.button_listener_finish);
+        button_finish.setVisibility(View.INVISIBLE);
+        button_finish.setEnabled(false);
+
+        button_reset = findViewById(R.id.button_reset);
+        this.button_listener_reset = new ButtonListenerReset();
+        button_reset.setOnTouchListener(this.button_listener_reset);
+        button_reset.setVisibility(View.VISIBLE);
+        button_reset.setEnabled(true);
+
+        this.training_listener = new TrainingListener();
+        mivry.SetTrainingListener(this.training_listener);
+    }
+
+    public class ButtonListenerRecord implements View.OnTouchListener {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            int action = event.getActionMasked();
+            switch (action) {
+                case MotionEvent.ACTION_DOWN:
+                    mivry.StartGesture(recording_gesture_id);
+                    mivry.SetContinuousIdentificationListener(MainActivity.this);
+                    button_record.setBackgroundColor(0xFFFF0000);
+                    return true;
+                case MotionEvent.ACTION_MOVE:
+                    final Rect r = new Rect();
+                    v.getHitRect(r);
+                    final float x = event.getX() + r.left;
+                    final float y = event.getY() + r.top;
+                    if (!r.contains((int) x, (int) y)) {
+                        mivry.CancelGesture();
+                        mivry.SetContinuousIdentificationListener(null);
+                        button_record.setBackgroundColor(0xFFCCCCCC);
+                    }
+                    return true;
+                case MotionEvent.ACTION_UP:
+                    break; // additional action required - will be performed below
+                default:
+                    return true; // ignore other events
+            }
+            // if we arrive here, then a gesture was finished
+            button_record.setBackgroundColor(0xFFCCCCCC);
+            mivry.SetContinuousIdentificationListener(null);
+            mivry.CancelGesture();
+            if (recording_gesture_id >= 0) {
+                int n = mivry.GetGestureNumberOfSamples(recording_gesture_id);
+                String c = mivry.GetGestureName(recording_gesture_id);
+                textview_message.setText("Recording gestures for keyword:\n" + c);
+                textview_keyword.setText(String.format("Number of recorded samples: %d", n));
+                if (n >= 10) { // minimum number of samples enforced
+                    button_finish.setEnabled(true);
+                    button_finish.setVisibility(View.VISIBLE);
+                    button_finish.setText("Finish recording&\nStart training");
+                    button_create.setEnabled(true);
+                    button_create.setVisibility(View.VISIBLE);
+                    button_create.setText("Tap to record\nanother gesture");
+                }
+            }
+            return true;
+        }
+    }
+
+    public class ButtonListenerFinish implements View.OnTouchListener {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            int action = event.getActionMasked();
+            if (action != MotionEvent.ACTION_UP) {
+                return true;
+            }
+            if (mivry.IsTraining()) {
+                mivry.StopTraining();
+                recording_gesture_id = -1;
+                textview_message.setText("Training stopped.\nFinal performance:");
+                textview_keyword.setText(String.format("(%.1f%%)", mivry.RecognitionScore()*100.0));
+                button_record.setEnabled(true);
+                button_record.setVisibility(View.VISIBLE);
+                button_record.setText("Touch and hold\nand move phone\nto perform gesture");
+                button_create.setEnabled(true);
+                button_create.setVisibility(View.VISIBLE);
+                button_create.setText("Tap to create\nnew gesture");
+                button_finish.setEnabled(true);
+                button_finish.setVisibility(View.VISIBLE);
+                button_finish.setText("Restart training");
+            } else {
+                mivry.SetMaxTrainingTime(60);
+                mivry.StartTraining();
+                recording_gesture_id = -1;
+                textview_message.setText("Training...");
+                textview_keyword.setText(String.format("(%.1f%%)", 0.0));
+                button_record.setEnabled(false);
+                button_record.setVisibility(View.INVISIBLE);
+                button_create.setEnabled(false);
+                button_create.setVisibility(View.INVISIBLE);
+                button_finish.setEnabled(true);
+                button_finish.setVisibility(View.VISIBLE);
+                button_finish.setText("Stop training");
+            }
+            return true;
+        }
+    }
+
+
+    public class ButtonListenerCreate implements View.OnTouchListener {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            int ret;
+            int action = event.getActionMasked();
+            if (action != MotionEvent.ACTION_UP) {
+                return true;
+            }
+            String c = getRandomCodeword();
+            recording_gesture_id = mivry.CreateGesture(c);
+
+            textview_message.setText("Recording new gesture.\nKeyword: "+c);
+            textview_keyword.setText("\"Number of recorded samples: 0");
+            button_record.setEnabled(true);
+            button_record.setVisibility(View.VISIBLE);
+            button_record.setText("Touch and hold\nand move phone\nto record gesture sample");
+            Resources resources = getResources();
+            layout_main.setBackgroundColor(resources.getColor(resources.getIdentifier(c, "color", getPackageName())));
+
+            button_create.setEnabled(false);
+            button_create.setVisibility(View.INVISIBLE);
+
+            button_finish.setEnabled(false);
+            button_finish.setVisibility(View.INVISIBLE);
+            return true;
+        }
+    }
+
+
+    public class ButtonListenerReset implements View.OnTouchListener {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            int action = event.getActionMasked();
+            if (action != MotionEvent.ACTION_UP) {
+                return true;
+            }
+            mivry.DeleteAllGestures();
+            init();
+            return true;
+        }
+    }
+
+    public class TrainingListener implements MiVRyTrainingListener {
+        @Override
+        public void trainingUpdateCallback(double performance)
+        {
+            recognition_preformance = performance;
+            runOnUiThread(new Runnable() { public void run() {
+                textview_keyword.setText(String.format("(%.1f%%)", recognition_preformance * 100.0));
+            }});
+        }
+        @Override
+        public void trainingFinishCallback(double performance)
+        {
+            android.text.format.DateFormat df = new android.text.format.DateFormat();
+            CharSequence date = df.format("yyyy-MM-dd_HHmmss", new java.util.Date());
+            boolean success = mivry.SaveToFile(save_gesture_database_path + "/MiVRy" + date + ".dat");
+            if (!success) {
+                Log.d("MiVRyDemo", "Failed to save gesture database to "+ save_gesture_database_path);
+            }
+            recognition_preformance = performance;
+            recording_gesture_id = -1;
+            runOnUiThread(new Runnable() { public void run() {
+                textview_message.setText("Training finished!\nFinal performance:");
+                textview_keyword.setText(String.format("%.1f%%",recognition_preformance * 100.0));
+                button_finish.setVisibility(View.VISIBLE);
+                button_finish.setEnabled(true);
+                button_finish.setText("Restart training");
+                button_create.setVisibility(View.VISIBLE);
+                button_create.setEnabled(true);
+                button_create.setText("Tap to create\nanother gesture");
+                button_record.setVisibility(View.VISIBLE);
+                button_record.setEnabled(true);
+                button_record.setText("Touch and hold\nand move phone\nto perform gesture");
+                String gesture_list = "Recorded gestures:\n";
+                for (int i = mivry.NumberOfGestures()-1; i>=0; i--) {
+                    gesture_list = gesture_list + mivry.GetGestureName(i) + "\n";
+                }
+                textview_gesturelist.setText(gesture_list);
+                textview_gesturelist.setVisibility(View.VISIBLE);
+                Resources resources = getResources();
+                layout_main.setBackgroundColor(resources.getColor(resources.getIdentifier("white", "color", getPackageName())));
+            }});
+        }
+    }
+
+    @Override
+    public void continuousIdentificationCallback(MiVRy.GestureRecognitionResult result)
+    {
+        String gesture_name = mivry.GetGestureName(result.gesture_id);
+        textview_message.setText("Identifying gesture:\n" + gesture_name);
+        textview_keyword.setText(String.format("(Confidence: %.1f%%)", result.similarity * 100.0));
+        if (result.gesture_id >= 0) {
+            Resources resources = getResources();
+            layout_main.setBackgroundColor(resources.getColor(resources.getIdentifier(gesture_name, "color", getPackageName())));
+        }
+    }
+
+    @Override
+    public void continuousRecordingCallback(int gesture_id, int samples_recorded)
+    {
+        textview_message.setText("Recoding gesture:\n" + mivry.GetGestureName(gesture_id));
+        textview_keyword.setText("(" + samples_recorded + " samples recorded)");
+    }
+}
--- android/README.md
+++ android/README.md
@@ -1,9 +1,10 @@
-# MiVRy 3D Gesture Recognition AI
+# MiVRy 3D Gesture Recognition AI - Android Integration
 Copyright (c) 2020 MARUI-PlugIn (inc.)
 
-MiVRy is an artificial intelligence (AI) that can learn to detect and distinguish motion patterns. It is aimed at human gesture recognition and can be trained to understand gestures such as a *"come here"* wave, performing a dance move, using tactical signs and gestures as used by military and police or you very own secret sign language.
+This is the android package (AAR) version of MiVRy.
 
-It is cross-platform and can be used with any VR device, mobile device (phone, smart watch) or with camera-based tracking systems (kinect).
+This repository contains the MiVRy gesture recognition package as well as sample applications that illustrate how MiVRy can be used on android devices such as smart phones or smart watches.
+
 
 ### License:
 <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png" /></a><br />MiVRy is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Attribution-NonCommercial 4.0 International License</a>.
@@ -14,4 +15,38 @@ You may not sell or rent any software that includes this software in part or in 
 **If you wish to use this software in a commercial application, please contact us to obtain a commercial license.**
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY  OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+### HOW TO USE:
+
+(1) Import the library into your project:
+(1.1) Select File > New > New Module.
+(1.2) Select Import .JAR/.AAR Package then click Next.
+(1.3) Select the location of MiVRy.aar and click "Finish".
+
+(2) Include in your java files:
+(2.1) Add the import to the top of your file:
+      import com.maruiplugin.mivry.MiVRy;
+(2.2) Create a MiVRy object:
+      MiVRy mivry = new MiVRy();
+
+(3) Create gestures:
+(3.1) Register a new gesture with:
+      int gesture_id = mivry.CreateGesture("my gesture");
+(3.2) Start the gesture recording with:
+      mivry.StartGesture(gesture_id);
+(3.3) Move the device to perform the gesture.
+(3.4) Finish the recording with:
+      mivry.EndGesture();
+(3.5) Repeat the process (3.2 ~ 3.4) multiple times. We recommend recording at least 20 samples.
+
+(4) Identify gestures:
+(4.1) Start the gesture with:
+      mivry.StartGesture(-1);
+(4.2) Move the device to perform the gesture.
+(4.3) End the gesture and get the ID of the identified gesture with
+      int gesture_id = mivry.EndGesture();
+(4.4) If needed, the name of the identified gesture can be acquired with
+      String gesture_name = mivry.GetGestureName(gesture_id);
+
 
