aeec6006c457438ebe6761e47e791ba74543a2ca
Max Krichenbauer <max@marui-plugin.com>
2020-06-07 13:03:08 +0900

    Fixed issue with task starting in the android widget.

--- android/MiVRyWidget/app/src/main/java/com/maruiplugin/mivrywidget/Item.java
+++ android/MiVRyWidget/app/src/main/java/com/maruiplugin/mivrywidget/Item.java
@@ -39,23 +39,39 @@ public class Item {
         }
         switch (this.type) {
             case WebLink:
-                Uri webpage = Uri.parse(this.uri); // "http://www.android.com"
+                Uri webpage = this.uri.matches("^https?://.*")
+                            ? Uri.parse(this.uri)
+                            : Uri.parse("http://" + this.uri); // "http://www.android.com"
                 Intent web_intent = new Intent(Intent.ACTION_VIEW, webpage);
+                web_intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 context.startActivity(web_intent);
                 return true;
             case CallContact:
                 Uri number = Uri.parse(this.uri); // "tel:5551234"
                 Intent call_intent = new Intent(Intent.ACTION_DIAL, number);
+                call_intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 context.startActivity(call_intent);
                 return true;
             case MapLocation:
-                Uri location = Uri.parse(this.uri); // "geo:0,0?q=1600+Amphitheatre+Parkway,+Mountain+View,+California"
-                // Or map point based on latitude/longitude: "geo:37.422219,-122.08364?z=14" where z param is zoom level
+                String maps_uri = "";
+                if (this.uri.matches("^geo:.*")) {
+                    maps_uri = this.uri;
+                } else {
+                    if (this.uri.matches("^-?[0-9.]*,-?[0-9.]*$")) {
+                        // map point based on latitude/longitude: "geo:37.422219,-122.08364?z=14" where z param is zoom level
+                        maps_uri = "geo:" + this.uri;
+                    } else { // search string: "geo:0,0?q=1600+Amphitheatre+Parkway,+Mountain+View,+California"
+                        maps_uri = "geo:0,0?q=" + this.uri.replaceAll(" ", "+");
+                    }
+                }
+                Uri location = Uri.parse(maps_uri);
                 Intent map_intent = new Intent(Intent.ACTION_VIEW, location);
+                map_intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 context.startActivity(map_intent);
                 return true;
             case AppLaunch:
                 Intent app_intent = context.getPackageManager().getLaunchIntentForPackage(this.uri); // "com.linkedin.android"
+                app_intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 context.startActivity(app_intent);
                 return true;
         }
